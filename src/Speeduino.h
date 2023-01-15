8f5c11ee1a72a1c2816ac092f083ed6fe548b884
Bexin3 <88098139+Bexin3@users.noreply.github.com>
2023-01-15 14:08:20 +0000

    Update Speeduino.h

--- src/Speeduino.h
+++ src/Speeduino.h
@@ -6,7 +6,7 @@
 void ADCSetup(bool DacRef, int Res, int Samp, int ADCClk, int ADCDiv, int BaseV, bool Freerun, bool PreDiv);
 void genericClockSetup(int clk, int dFactor);
 void AttachClock(int clk, int clkid);
-void AttachADC(int ADCpin = 0, int gain = 1, bool IDACRefon = 0);  
+void AttachADC(int ADCpin = 0, bool IDACRefon = 0, int gain = 1);  
 void GainINIT(int Gain); 
 void DACSetup(int BaseV);
 void AnalogBegin(int resolution = 12, bool midphase = 0, bool Freerun = 0);
