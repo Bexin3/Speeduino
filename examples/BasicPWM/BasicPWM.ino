0d6264821f25beb5d2e59b24d3e10eb47612c7ee
Bexin3 <88098139+Bexin3@users.noreply.github.com>
2023-01-15 12:00:13 +0000

    Update BasicPWM.ino

--- examples/BasicPWM/BasicPWM.ino
+++ examples/BasicPWM/BasicPWM.ino
@@ -11,6 +11,8 @@ PWMBegin(pin, frequency);
 void loop() {
 PWMDuty(13, 0.2f); //second number is the duty cycle fraction
 delay(10000);
-PWMDuty(13, 1.0f);
+PWMFrequency(13, 20.0f);
+PWMDuty(13, 0.8f);
 delay(10000);
+PWMFrequency(13, 2.0f);
 }
