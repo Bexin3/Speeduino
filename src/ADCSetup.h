435105db4266843f3c6807d35336ef86d366b147
Bexin3 <88098139+Bexin3@users.noreply.github.com>
2023-01-15 14:31:04 +0000

    Update ADCSetup.h

--- src/ADCSetup.h
+++ src/ADCSetup.h
@@ -7,6 +7,7 @@ void ADCSetup(bool DacRef, int Res, int Samp, int ADCClk, int ADCDiv, int BaseV,
 void genericClockSetup(int clk, int dFactor);
 void AttachClock(int clk, int clkid);
 void AttachADC(int ADCpin = 0, bool IDACRefon = 0, int gain = 1);  
+void FastAttachADC(int ADCpin = 0, bool IDACRefon = 0);
 void GainINIT(int Gain); 
 void DACSetup(int BaseV);
 void AnalogBegin(int resolution = 12, bool midphase = 0, bool Freerun = 0);
